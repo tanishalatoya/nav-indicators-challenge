9952c7b88f66fbcbf4aff59189acc5ead0c60cdc
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-20 10:16:59 -0700

    Add functionality to highlight each link selection

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 var navigationBar = document.querySelector('nav');
+var main = document.querySelector('main');
 
 navigationBar.addEventListener('click', highlightSelection);
 
-function highlightSelection() {
-  var listItem = document.querySelectorAll('li');
+function highlightSelection(event) {
+    var allCourses = document.querySelector('#all-courses');
+    var collections = document.querySelector('#collections');
+    var wishlist = document.querySelector('#wishlist');
+    var archived = document.querySelector('#archived');
 
-  for (var i = 0; i < listItem.length; i++) {
-    if (event.target === listItem[i]) {
-      listItem[i].classList.add('highlight');
+    if (event.target.id === 'all-courses') {
+      allCourses.classList.add ('highlight');
+      collections.classList.remove('highlight');
+      whishlist.classList.remove('highlight');
+      archived.classList.remove('highlight');
+
+    } else if (event.target.id === 'collections') {
+      collections.classList.add ('highlight');
+      allCourses.classList.remove('highlight');
+      whishlist.classList.remove('highlight');
+      archived.classList.remove('highlight');
+
+    } else if (event.target.id === 'wishlist') {
+      wishlist.classList.add ('highlight');
+      allCourses.classList.remove('highlight');
+      collections.classList.remove('highlight');
+      archived.classList.remove('highlight');
+
+    } else {
+      archived.classList.add ('highlight');
+      allCourses.classList.remove('highlight');
+      collections.classList.remove('highlight');
+      wishlist.classList.remove('highlight');
     }
-  }
 }
