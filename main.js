6fa15339b8e5c3fa64fd366b3db51b9b042e822e
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-20 10:58:15 -0700

    Refactor highlightSelection function

--- main.js
+++ main.js
@@ -1,41 +1,55 @@
 var navigationBar = document.querySelector('nav');
 
-
 navigationBar.addEventListener('click', highlightSelection);
 navigationBar.addEventListener('click', displayInformation);
 
 
-function highlightSelection(event) {
-    var allCourses = document.querySelector('#all-courses');
-    var collections = document.querySelector('#collections');
-    var wishlist = document.querySelector('#wishlist');
-    var archived = document.querySelector('#archived');
+function highlightSelection() {
+  var navParagraphs = document.querySelectorAll('nav > p');
 
-    if (event.target.id === 'all-courses') {
-      allCourses.classList.add ('highlight');
-      collections.classList.remove('highlight');
-      wishlist.classList.remove('highlight');
-      archived.classList.remove('highlight');
-
-    } else if (event.target.id === 'collections') {
-      collections.classList.add ('highlight');
-      allCourses.classList.remove('highlight');
-      wishlist.classList.remove('highlight');
-      archived.classList.remove('highlight');
-
-    } else if (event.target.id === 'wishlist') {
-      wishlist.classList.add ('highlight');
-      allCourses.classList.remove('highlight');
-      collections.classList.remove('highlight');
-      archived.classList.remove('highlight');
-
+  for (var i = 0; i < navParagraphs.length; i++) {
+    if (event.target !== navParagraphs[i]) {
+      !navParagraphs[i].classList.remove('highlight');
     } else {
-      archived.classList.add ('highlight');
-      allCourses.classList.remove('highlight');
-      collections.classList.remove('highlight');
-      wishlist.classList.remove('highlight');
+      navParagraphs[i].classList.add('highlight')
     }
+  }
 }
+
+// function highlightSelection(event) {
+//     var allCourses = document.querySelector('#all-courses');
+//     var collections = document.querySelector('#collections');
+//     var wishlist = document.querySelector('#wishlist');
+//     var archived = document.querySelector('#archived');
+//
+//     if (event.target.id === 'all-courses') {
+//       allCourses.classList.add ('highlight');
+//       collections.classList.remove('highlight');
+//       wishlist.classList.remove('highlight');
+//       archived.classList.remove('highlight');
+//
+//     } else if (event.target.id === 'collections') {
+//       collections.classList.add ('highlight');
+//       allCourses.classList.remove('highlight');
+//       wishlist.classList.remove('highlight');
+//       archived.classList.remove('highlight');
+//
+//     } else if (event.target.id === 'wishlist') {
+//       wishlist.classList.add ('highlight');
+//       allCourses.classList.remove('highlight');
+//       collections.classList.remove('highlight');
+//       archived.classList.remove('highlight');
+//
+//     } else {
+//       archived.classList.add ('highlight');
+//       allCourses.classList.remove('highlight');
+//       collections.classList.remove('highlight');
+//       wishlist.classList.remove('highlight');
+//     }
+// }
+
+
+// Refactor this to use a variable to pull in teh various messages.
 
 function displayInformation(event) {
   var main = document.querySelector('main');
